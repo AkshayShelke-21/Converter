2e7200d308c05d7f96fdfc3e3ea3a46703289371
AkshayShelke21 <akshelke21@gmail.com>
2025-01-20 16:09:19 +0530

    Chunk Size issue resolved

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Example: Split vendor libraries into their own chunk
+          if (id.includes("node_modules")) {
+            return "vendor"; // All node_modules will be in the vendor chunk
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 2000, // Adjust the chunk size limit if needed
+  },
+});
